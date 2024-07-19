90f8464392fd9df71d14c9b83cd08e415bdccb1c
mohamed-elsisi1 <36666159+MohamedELsisi@users.noreply.github.com>
2024-07-19 17:48:48 +0300

    Update OrderRepository.java

--- src/main/java/com/qeema/engineering/repository/OrderRepository.java
+++ src/main/java/com/qeema/engineering/repository/OrderRepository.java
@@ -2,7 +2,9 @@ package com.qeema.engineering.repository;
 
 import com.qeema.engineering.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    
+
 }
