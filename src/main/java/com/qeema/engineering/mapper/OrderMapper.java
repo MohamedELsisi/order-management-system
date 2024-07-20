f43a70607a99b46951abd120dcd7ec490e60dfa6
mohamed-elsisi1 <36666159+MohamedELsisi@users.noreply.github.com>
2024-07-20 04:03:02 +0300

    Create OrderMapper.java

---
+++ src/main/java/com/qeema/engineering/mapper/OrderMapper.java
@@ -0,0 +1,24 @@
+package com.qeema.engineering.mapper;
+
+
+import com.qeema.engineering.dto.OrderDTO;
+import com.qeema.engineering.model.Order;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+
+@Mapper(componentModel = "spring", unmappedSourcePolicy = ReportingPolicy.IGNORE)
+public interface OrderMapper {
+
+
+    @BeanMapping(ignoreByDefault = true)
+    @Mapping(target = "productList", source = "products")
+    OrderDTO mapFromOrderEntityToOrderDTO(Order order);
+
+    @BeanMapping(ignoreByDefault = true)
+    @Mapping(target = "products", source = "productList")
+   Order mapFromOrderDtoToOrderEntity(OrderDTO  orderDTO);
+}
+
