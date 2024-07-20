57c75daf72160de844117eb6154840d877d14b3c
mohamed-elsisi1 <36666159+MohamedELsisi@users.noreply.github.com>
2024-07-20 17:44:39 +0300

    add logging to handlers and services

--- src/main/java/com/qeema/engineering/service/impl/OrderServiceImpl.java
+++ src/main/java/com/qeema/engineering/service/impl/OrderServiceImpl.java
@@ -1,31 +1,33 @@
 package com.qeema.engineering.service.impl;
 
 import com.qeema.engineering.dto.OrderDTO;
-import com.qeema.engineering.dto.ProductDTO;
 import com.qeema.engineering.exception.ResourceException;
 import com.qeema.engineering.mapper.OrderMapper;
 import com.qeema.engineering.model.Order;
 import com.qeema.engineering.model.Product;
 import com.qeema.engineering.repository.OrderRepository;
-import com.qeema.engineering.repository.ProductRepository;
 import com.qeema.engineering.service.OrderService;
 import com.qeema.engineering.service.ProductService;
 import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 
 @Service
 public class OrderServiceImpl implements OrderService {
+    private static final Logger logger = LoggerFactory.getLogger(OrderServiceImpl.class.getName());
 
     OrderRepository orderRepository;
     ProductService productService;
     OrderMapper orderMapper;
+
 
 
     public OrderServiceImpl(OrderRepository orderRepository, ProductService productService, OrderMapper orderMapper) {
@@ -38,18 +40,24 @@ public class OrderServiceImpl implements OrderService {
     @Override
     public CompletableFuture<Void> addOrder(OrderDTO orderDTO) {
         List<Product> productsToUpdate = new ArrayList<>();
-
+        logger.info("creating new order");
         return CompletableFuture.runAsync(() -> {
-            
+
             orderDTO.getProductList().forEach(productDTO -> {
-                Product existingProduct = productService.getProductByID(productDTO.getId())
-                        .orElseThrow(() -> new ResourceException("Product Not Found"));
+                logger.info("get product {} from database", productDTO.getId());
+
+                Product existingProduct = productService.getProductByID(productDTO.getId()).orElseThrow(() -> {
+                    logger.info("Product {} not found", productDTO.getId());
+                    return new ResourceException("Product Not Found with ID: " + productDTO.getId());
+                });
 
                 if (productDTO.getQuantity() >= existingProduct.getQuantity()) {
+                    logger.error("product {} is over quantity", productDTO.getId());
                     throw new ResourceException("Product quantity is less than the requested quantity");
                 }
 
                 existingProduct.setQuantity(existingProduct.getQuantity() - productDTO.getQuantity());
+
                 productsToUpdate.add(existingProduct);
             });
 
@@ -62,18 +70,19 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public List<OrderDTO> getOrders() {
-        return orderRepository.findAll().stream()
-                .map(orderMapper::mapFromOrderEntityToOrderDTO)
-                .collect(Collectors.toList());
+        logger.error("get all orders");
+        return orderRepository.findAll().stream().map(orderMapper::mapFromOrderEntityToOrderDTO).collect(Collectors.toList());
     }
 
 
     @Transactional
     protected void handelCreatTheOrder(Order order) {
+        logger.info("save the new order");
         orderRepository.save(order);
     }
 
     private void updateTheProducts(List<Product> products) {
+        logger.info("update the products");
         productService.updateProducts(products);
     }
 
